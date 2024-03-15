d7bf145f9f250480e60ff358ecfda10b8d167496
Rodrigoneal <rodrigho2006@gmail.com>
2024-03-15 15:54:08 -0300

    Colocando o tamanho da string do sql

--- sumula/api/config/db.py
+++ sumula/api/config/db.py
@@ -2,13 +2,10 @@ import os
 from functools import cache
 from typing import AsyncGenerator
 
-import motor.motor_asyncio
-from beanie import init_beanie
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from sumula.api.domain.model import reg
-from sumula.entities.entities import Sumula
 
 num_cpu = os.cpu_count()
 
@@ -19,7 +16,6 @@ load_dotenv()
 
 class Settings:
     asyncpg_url: str = os.getenv("SQL_URL") or "sqlite+aiosqlite:///agendamento.db"
-    # MONGO_URI: str = os.getenv("MONGO_URI") or "mongodb://localhost:27017"
 
 
 @cache
@@ -38,7 +34,6 @@ settings = get_settings()
 engine = create_async_engine(settings.asyncpg_url)
 
 
-# async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
@@ -49,8 +44,3 @@ async def create_tables():
     """Cria as tabelas no banco de dados."""
     async with engine.begin() as conn:
         await conn.run_sync(reg.metadata.create_all)
-
-
-async def init():
-    client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_URI)
-    await init_beanie(database=client.sumula, document_models=[Sumula])
--- sumula/api/domain/model.py
+++ sumula/api/domain/model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import JSON, func
+from sqlalchemy import JSON, func, String
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
 
@@ -14,20 +14,20 @@ class AuthenticationModel:
     id: Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True)
     id_account: Mapped[int] = mapped_column(nullable=False)
     codigo_cliente: Mapped[int] = mapped_column(nullable=False)
-    razao_social: Mapped[str] = mapped_column(nullable=True)
-    nick_name: Mapped[str] = mapped_column(nullable=True)
-    cnpj: Mapped[str] = mapped_column(nullable=True)
-    tipo_empresa: Mapped[str] = mapped_column(nullable=True)
-    uf: Mapped[str] = mapped_column(nullable=True)
-    chave_api_1: Mapped[str] = mapped_column(nullable=True)
-    chave_api_2: Mapped[str] = mapped_column(nullable=True)
-    chave_api_3: Mapped[str] = mapped_column(nullable=True)
-    codigo_generico_1: Mapped[str] = mapped_column(nullable=True)
-    codigo_generico_2: Mapped[str] = mapped_column(nullable=True)
+    razao_social: Mapped[str] = mapped_column(String(255), nullable=True)
+    nick_name: Mapped[str] = mapped_column(String(255), nullable=True)
+    cnpj: Mapped[str] = mapped_column(String(255), nullable=True)
+    tipo_empresa: Mapped[str] = mapped_column(String(255), nullable=True)
+    uf: Mapped[str] = mapped_column(String(255), nullable=True)
+    chave_api_1: Mapped[str] = mapped_column(String(255), nullable=True)
+    chave_api_2: Mapped[str] = mapped_column(String(255), nullable=True)
+    chave_api_3: Mapped[str] = mapped_column(String(255), nullable=True)
+    codigo_generico_1: Mapped[str] = mapped_column(String(255), nullable=True)
+    codigo_generico_2: Mapped[str] = mapped_column(String(255), nullable=True)
     modules: Mapped[dict | list] = mapped_column(JSON, nullable=True)
     subModules: Mapped[dict | list] = mapped_column(JSON, nullable=True)
     status: Mapped[bool] = mapped_column(nullable=False)
-    created_by: Mapped[str] = mapped_column(nullable=True)
+    created_by: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         init=False, onupdate=func.now(), nullable=True
@@ -39,13 +39,13 @@ class SumulaModel:
     __tablename__ = "api_sumula"
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True)
-    competicao: Mapped[str] = mapped_column()
-    rodada: Mapped[str] = mapped_column()
-    data: Mapped[str] = mapped_column()
-    horario: Mapped[str] = mapped_column()
-    estadio: Mapped[str] = mapped_column()
-    mandante: Mapped[str] = mapped_column()
-    visitante: Mapped[str] = mapped_column()
+    competicao: Mapped[str] = mapped_column(String(255), nullable=True)
+    rodada: Mapped[str] = mapped_column(String(255), nullable=True)
+    data: Mapped[str] = mapped_column(String(255), nullable=True)
+    horario: Mapped[str] = mapped_column(String(255), nullable=True)
+    estadio: Mapped[str] = mapped_column(String(255), nullable=True)
+    mandante: Mapped[str] = mapped_column(String(255), nullable=True)
+    visitante: Mapped[str] = mapped_column(String(255), nullable=True)
     ano: Mapped[int] = mapped_column()
     jogo_num: Mapped[int] = mapped_column()
     jogo: Mapped[str] = mapped_column(JSON)
@@ -61,7 +61,7 @@ class SumulaModel:
     observacao: Mapped[dict[str, str]] = mapped_column(JSON)
     assistente: Mapped[dict[str, str]] = mapped_column(JSON)
     substituicao: Mapped[list[dict[str, str]]] = mapped_column(JSON)
-    url_pdf: Mapped[str] = mapped_column()
+    url_pdf: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         init=False, onupdate=func.now(), nullable=True
@@ -76,8 +76,8 @@ class Logs:
     id_client: Mapped[int] = mapped_column()
     dh_chamada: Mapped[datetime] = mapped_column()
     dh_retorno: Mapped[datetime] = mapped_column()
-    url_chamada: Mapped[str] = mapped_column()
+    url_chamada: Mapped[str] = mapped_column(String(255), nullable=True)
     st_sucesso: Mapped[bool] = mapped_column()
     dados_retorno: Mapped[dict] = mapped_column(JSON)
     cd_ocorrencia: Mapped[int] = mapped_column()
-    ds_erro: Mapped[str] = mapped_column()
+    ds_erro: Mapped[str] = mapped_column(String(255), nullable=True)
